505c635befc4b5fb1e9f31d4d9be08e712b8b213
Terbuh <hubertt.popowicz@gmail.com>
2021-12-10 22:03:42 +0100

    initial commit

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import 'typeface-roboto';
 import './index.css';
-
-import App from './App';
+import App from './app/components/App/index';
 
 ReactDOM.render(<App />, document.getElementById('root'));
