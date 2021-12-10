63cd031591c39fcb06c67397187aca37affcdf5b
Terbuh <hubertt.popowicz@gmail.com>
2021-12-10 15:49:07 +0100

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Questionaire from './app/components/Questionaire';
+import Menu from './app/components/Menu';
+
+export default function App() {
+  return (
+    <div>
+      <Menu />
+      <Questionaire />
+    </div>
+  );
+}
---
+++ src/app/components/Questionaire.tsx
@@ -0,0 +1,43 @@
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import TextField from '@mui/material/TextField';
+import './Questionaire.css';
+import ChooseService from './ChooseService';
+import ChooseRepair from './ChooseRepair';
+import Button from '@mui/material/Button';
+// import CarDate from './ChooseYear';
+
+export default function Questionaire() {
+  return (
+    <div className="Box-wrapper">
+      <Box
+        component="form"
+        sx={{
+          borderRadius: '10%',
+          backgroundColor: 'rgba(0,0,0,0.8)',
+          width: 450
+        }}
+        noValidate
+        autoComplete="off">
+        <div className="content">
+          <h2>Workshop</h2>
+          <ChooseService />
+          <h2>Services</h2>
+
+          <ChooseRepair />
+          <TextField required id="outlined-basic" label="Type your car brand" />
+          <TextField required id="outlined-basic" label="Type your car model" />
+          <TextField required id="outlined-basic" label="VIN" />
+          <TextField required id="outlined-basic" label="Engine capacity" />
+          <TextField
+            required
+            id="outlined-basic"
+            type="number"
+            label="Year - Datepicker myślę"
+          />
+          <Button variant="contained">Check</Button>
+        </div>
+      </Box>
+    </div>
+  );
+}
