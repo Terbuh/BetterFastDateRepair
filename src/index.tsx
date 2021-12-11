0b89bc2d8231dab3ffaf8edbdb136649dc50ccd7
Terbuh <hubertt.popowicz@gmail.com>
2021-12-11 20:36:01 +0100

    feat(index): add styles

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import 'typeface-roboto';
-import './index.css';
+import './index_style.css';
 import App from './App';
 
 ReactDOM.render(<App />, document.getElementById('root'));
