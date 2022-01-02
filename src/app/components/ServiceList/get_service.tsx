838826e3d1870f3063ff8d3c6804697ae90fb091
Terbuh <hubertt.popowicz@gmail.com>
2022-01-02 13:37:13 +0100

    Add code of list with services + styled

--- src/app/components/ServiceList/get_service.tsx
+++ src/app/components/ServiceList/get_service.tsx
@@ -1,14 +1,28 @@
 import React from 'react';
 import services from '../__mock__/data';
+import RenderList from './get_service_styles';
 
-const renderService = async () => {
-  let template = '';
-  services.forEach((service) => {
-    template += `
-                <table>
-                <th>Name: ${service.name}</th>
-                 </table>`;
-  });
-};
+function RenderService() {
+  return (
+    <RenderList>
+      {services.map((item) => (
+        <div key={item.id} id="post">
+          <h3>{item.name}</h3>
+          <p>{item.open}</p>
+        </div>
+      ))}
+    </RenderList>
+  );
+}
 
-export default renderService;
+export default RenderService;
+
+// const RenderService = async () => {
+//   let template = '';
+//   services.forEach((service) => {
+//     template += `
+//                 <table>
+//                 <th>Name: ${service.name}</th>
+//                  </table>`;
+//   });
+// };
---
+++ src/app/components/ServiceList/get_service_styles.ts
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+
+const RenderList = styled.div`
+  width: 700px;
+  height: 700px;
+  overflow-y: scroll;
+`;
+
+export default RenderList;
