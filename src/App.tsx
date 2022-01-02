4b568afbdef1308d4b01fa0997b19e4b405b4c7b
Terbuh <hubertt.popowicz@gmail.com>
2022-01-02 13:36:36 +0100

    Prettier

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,12 @@ import React from 'react';
 import FullFrom from './app/components/ApplicationForm/ApplicationForm';
 import Menu from './app/components/Menu/menu';
 import Container from './app_style';
-import renderService from './app/components/ServiceList/get_service';
+import RenderService from './app/components/ServiceList/get_service';
 
 const App: React.FC = () => {
   return (
     <Container>
-      <renderService />
+      <RenderService />
       <Menu />
       <FullFrom />
     </Container>
