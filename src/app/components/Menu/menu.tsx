5288dc91ca479be3f208c5e9015cd3c039dcf780
Terbuh <hubertt.popowicz@gmail.com>
2022-01-10 21:07:22 +0100

    Add new Menu styles + new Div

--- src/app/components/Menu/menu.tsx
+++ src/app/components/Menu/menu.tsx
@@ -1,15 +1,19 @@
 import React from 'react';
-// import  from './menu_style';
+import ButtonGroupWrapper from './menu.style';
 import Button from '@mui/material/Button';
 import ButtonGroup from '@mui/material/ButtonGroup';
 
 export default function Menu() {
   return (
-    <ButtonGroup variant="contained" aria-label="outlined primary button group">
-      <Button>Login</Button>
-      <Button>Register</Button>
-      <Button>Contact</Button>
-      <Button>About Us</Button>
-    </ButtonGroup>
+    <ButtonGroupWrapper>
+      <ButtonGroup
+        variant="contained"
+        aria-label="outlined primary button group">
+        <Button>Login</Button>
+        <Button>Register</Button>
+        <Button>Contact</Button>
+        <Button>About Us</Button>
+      </ButtonGroup>
+    </ButtonGroupWrapper>
   );
 }
