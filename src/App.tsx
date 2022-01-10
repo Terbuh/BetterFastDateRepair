fc9d9f926c61938b72567fcd2d744f7cf28e4723
Terbuh <hubertt.popowicz@gmail.com>
2022-01-10 21:06:19 +0100

    Add Map

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,18 @@
 import React from 'react';
 //import FullFrom from './app/components/ApplicationForm/application_form';
 import Menu from './app/components/Menu/menu';
-import Container from './app.style';
+import { Container, Header, WebLogo } from './app.style';
 import Service from './app/components/ServiceList/service';
+import Map from './app/components/Map/map';
 
 const App: React.FC = () => {
   return (
     <Container>
-      <Menu />
+      <Header>
+        <WebLogo>Logo</WebLogo>
+        <Menu />
+      </Header>
+      <Map />
       <Service />
 
       {/*//<FullFrom />*/}
