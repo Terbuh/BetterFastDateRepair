1d2b81b81ec353a6897539116ce6a436983c6f27
Terbuh <hubertt.popowicz@gmail.com>
2022-01-23 11:16:59 +0100

    feat ( service.style ): delete scrollbar

--- src/app/components/ServiceList/service.styles.ts
+++ src/app/components/ServiceList/service.styles.ts
@@ -1,9 +1,12 @@
 import styled from 'styled-components';
 
 const ListHeight = styled.div`
-  width: 800px;
+  width: 50%;
   height: 800px;
   overflow-y: scroll;
+  ::-webkit-scrollbar {
+    display: none;
+  }
   margin: 0;
   background-color: rgba(197, 196, 195, 0.37);
 `;
