d9b4d62ff9c19a45fe16a028e3f134a7de760a1d
Terbuh <hubertt.popowicz@gmail.com>
2022-01-25 13:41:11 +0100

    Feat (map): fighting

--- src/app/components/Map/map.styles.ts
+++ src/app/components/Map/map.styles.ts
@@ -3,6 +3,7 @@ import styled from 'styled-components';
 const MapWrapper = styled.div`
   width: 50%;
   height: 800px;
+  overflow: hidden;
   background-color: black;
 `;
 
--- src/app/components/Map/map.tsx
+++ src/app/components/Map/map.tsx
@@ -4,17 +4,12 @@ import { MapWrapper } from './map.styles';
 
 export const Map: React.FC = () => {
   return (
-    <MapWrapper>
-      <MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false}>
+    <MapWrapper className="leaflet-container">
+      <MapContainer center={[45.4, -75.7]} zoom={12} scrollWheelZoom={false}>
         <TileLayer
-          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
         />
-        <Marker position={[51.505, -0.09]}>
-          <Popup>
-            A pretty CSS3 popup. <br /> Easily customizable.
-          </Popup>
-        </Marker>
       </MapContainer>
     </MapWrapper>
   );
