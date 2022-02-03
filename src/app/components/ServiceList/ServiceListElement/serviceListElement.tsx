831648897a3426e66c9fd17ccadae2e0d7e8b49c
Terbuh <hubertt.popowicz@gmail.com>
2022-02-03 12:39:29 +0100

    Feat (Router): mentor changes

--- src/app/components/ServiceList/ServiceListElement/serviceListElement.tsx
+++ src/app/components/ServiceList/ServiceListElement/serviceListElement.tsx
@@ -7,21 +7,24 @@ import {
   MyP,
   MyItemFlex
 } from './serviceListElement.styles';
+import { Link } from 'react-router-dom';
 
 export const ServiceListElement = (serviceData: serviceType): JSX.Element => {
   return (
-    <MyListItemButton>
-      <Logo>{React.createElement(serviceData.icon)}</Logo>
-      <MyListItem>
-        <h2>{serviceData.name}</h2>
-      </MyListItem>
-      <MyItemFlex>
-        Main specialization in - <MyP>{serviceData.spec}</MyP>
-      </MyItemFlex>
-      <MyListItem>Service is open at: {serviceData.open}</MyListItem>
-      <MyItemFlex>
-        Email to contact: <MyP>{serviceData.email}</MyP>
-      </MyItemFlex>
-    </MyListItemButton>
+    <Link to="ServiceListElement:id">
+      <MyListItemButton>
+        <Logo>{React.createElement(serviceData.icon)}</Logo>
+        <MyListItem>
+          <h2>{serviceData.name}</h2>
+        </MyListItem>
+        <MyItemFlex>
+          Main specialization in - <MyP>{serviceData.spec}</MyP>
+        </MyItemFlex>
+        <MyListItem>Service is open at: {serviceData.open}</MyListItem>
+        <MyItemFlex>
+          Email to contact: <MyP>{serviceData.email}</MyP>
+        </MyItemFlex>
+      </MyListItemButton>
+    </Link>
   );
 };
