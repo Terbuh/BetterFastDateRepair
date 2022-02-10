3136bbc6071d9561ee398c9bc69ac5fafa523c6a
Terbuh <hubertt.popowicz@gmail.com>
2022-02-10 20:31:46 +0100

    Feat (async): fighting with async

--- src/app/components/ServiceDetails/async.tsx
+++ src/app/components/ServiceDetails/async.tsx
@@ -1,35 +1,9 @@
 import services from '../__mock__/mock';
 import { serviceType } from '../../types/service.type';
 
-const mockData = services;
+// const mockData = services;
 
-async function getDataUser(url: string): Promise<serviceType> {
-  const res = await fetch(url);
-  return res.json();
+export async function GetServiceData(id: number): Promise<serviceType[]> {
+  const userData = await services;
+  return userData;
 }
-
-const mockDataX = getDataUser(mockData);
-
-
-const render = async (): Promise<void> => {
-  const data = await mockDataX;
-
-  const template = '';
-
-
-    users.forEach((user) => {
-      if (company.uri === user.uris.company) {
-        template += `
-                  <table class="user">
-                  <td>Name: ${user.name},</td>
-                  <td>email: ${user.email}</td>
-                  </table>`;
-      }
-    });
-  });
-
-  if (container3 != null) {
-    container3.innerHTML = template;
-  }
-};
-renderCompaniesAndItsUsers();
--- src/app/components/ServiceDetails/serviceDetails.tsx
+++ src/app/components/ServiceDetails/serviceDetails.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { useParams } from 'react-router-dom';
 import { serviceType } from '../../types/service.type';
+import { GetServiceData } from './async';
 
 // interface Data {
 //   serviceData: serviceType;
@@ -10,6 +11,7 @@ export const ServiceDetails: React.FC = () => {
   const { id } = useParams();
   return (
     <div>
+      {/*<GetServiceData />*/}
       <h1>{id}</h1>
       <h2>Now showing post </h2>
     </div>
