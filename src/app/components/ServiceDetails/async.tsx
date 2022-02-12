4df64089902bae06baea2826c7462b5fca385e29
Hubert_Pop <hubertt.popowicz@gmail.com>
2022-02-12 12:40:03 +0100

    feat (async) : test

--- src/app/components/ServiceDetails/async.tsx
+++ src/app/components/ServiceDetails/async.tsx
@@ -1,9 +1,19 @@
 import services from '../__mock__/mock';
 import { serviceType } from '../../types/service.type';
+import React from 'react';
 
-// const mockData = services;
-
-export async function GetServiceData(id: number): Promise<serviceType[]> {
-  const userData = await services;
-  return userData;
+interface DetailsProps {
+  serviceDetails: serviceType;
 }
+export const Datex = () => {
+    return(
+  services.map((dane) => (<h1>{dane}</h1>));
+};
+
+// export async function MyFunction(): Promise<DetailsProps> {
+//     try {
+//         const date = await Datex();
+//         return (<div>
+//             {date}
+//         </div>);
+//     }}
