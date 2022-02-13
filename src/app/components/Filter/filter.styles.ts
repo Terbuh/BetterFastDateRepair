ae38cc4ff17577e5b745660086a01a1f10e6b345
Terbuh <hubertt.popowicz@gmail.com>
2022-02-13 18:00:55 +0100

    Feat (filter): starting styling placeholder

---
+++ src/app/components/Filter/filter.styles.ts
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+
+export const MySearch = styled.input`
+  font-size: 1.5em;
+
+  background: linear-gradient(21deg, #10abff, #1beabd);
+
+  padding: 3px;
+
+  display: inline-block;
+
+  border-radius: 9999em;
+`;
